35e55c09c6fbbd34f3b0279ffa23278ca37ea1bd
Eduard <eddnovik@gmail.com>
2022-10-07 03:42:41 +0200

    add lesson03

---
+++ Lesson 03/lesson03 script3.js
@@ -0,0 +1,3 @@
+const number1 = prompt('Please, type how many hours')
+const convertToSec = Number(number1)*3600
+alert(`Here is the value in seconds: ` + convertToSec)
---
+++ Lesson 03/lesson03 script4.js
@@ -0,0 +1,10 @@
+const number1 = prompt('Type first number')
+const number2 = prompt('Type second number')
+const number3 = prompt('Type third number')
+
+const arithmetic = (Number(number1) + Number(number2) + Number(number3))/3
+alert(`The arithmetic mean of numbers is: ` + arithmetic)
+
+
+
+
