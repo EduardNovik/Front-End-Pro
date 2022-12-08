482dced1c4a81ade2211ad6fac3fce1924d42320
Eduard <eddnovik@gmail.com>
2022-12-08 21:34:48 +0100

    add lesson20 hw01_v2

--- Lesson 20_HW01/lesson 20 script1.js
+++ Lesson 20_HW01/lesson 20 script1.js
@@ -18,8 +18,8 @@
   
 // Event Listeners
   
-  btnSearch.addEventListener('click', searchPost)
-  btnComments.addEventListener('click', () => {searchPost().then(post => addComments(post))})
+  btnSearch.addEventListener('click', () => {searchPost().catch(err => err)})
+  btnComments.addEventListener('click', () => {searchPost().then(post => addComments(post)).catch(err => err)})
   btnDelete.addEventListener('click', removeComments)
 
 
@@ -35,6 +35,7 @@ function searchPost() {
   btnComments.style.display = 'block'
   btnDelete.style.display = 'block'
   commentsBlock.innerHTML = ''
+  btnComments.removeAttribute('disabled')
   
   return new Promise((resolve, reject) => {
 
@@ -44,52 +45,58 @@ function searchPost() {
         .then((post) => {
           resolve(post);
           postBlock.innerHTML = `<b>Post title: ${post.title}</b><p>Post: ${post.body}</p><p>Post id:${post.id}</p>`;
-        }) 
-        .catch((err) => reject("Error is:", err));
+        })
+        .catch((err) => {
+          reject(err);
+          postBlock.innerText = `${err}`;
+          btnComments.style.display = "none";
+          btnDelete.style.display = "none";
+        });
     } else {
       btnComments.style.display = 'none'
       btnDelete.style.display = 'none'
       postBlock.innerText = "There is no such post, please type the number from 1 to 100.";
     }
   })
-  
 }
 
 
 
-function addComments (post) {
+function addComments(post) {
 
- return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     fetch(`https://jsonplaceholder.typicode.com/comments?postId=${post.id}`)
-    .then((response) => response.json())
-    .then(comments => {resolve (
-        comments.forEach(item => {
-        const commentItem = document.createElement('div')
-        commentItem.setAttribute('class', 'comment-item')
-        commentItem.innerHTML = 
-        `<p>Name: ${item.name}</p><p>Comment: ${item.body}</p>
-        <p>Email: ${item.email}</p><p>Post Id: ${item.postId}</p>`
-        commentsBlock.insertAdjacentElement('beforeend', commentItem)
-      }));
+      .then((response) => response.json())
+      .then((comments) => {
+        resolve(comments);
+        comments.forEach((item) => {
+          const createCommentItem = document.createElement("div");
+          createCommentItem.setAttribute("class", "comment-item");
+          createCommentItem.innerHTML = `<p>Name: ${item.name}</p><p>Comment: ${item.body}</p>
+          <p>Email: ${item.email}</p><p>Post Id: ${item.postId}</p>`;
+          commentsBlock.insertAdjacentElement("beforeend", createCommentItem);
+        });
       })
-    .catch(err => reject('Error is:', err))
-  })
+      .then(() => {
+        const commentItem = document.querySelectorAll(".comment-item");
+        if (commentItem !== null) {
+          btnComments.disabled = "true";
+        } else {
+          btnComments.removeAttribute("disabled");
+        }
+      })
+      .catch((err) => {
+        reject(err);
+        postBlock.innerText = `${err}`;
+        btnComments.style.display = "none";
+        btnDelete.style.display = "none";
+      });
+  });
 }
 
 
 
- function removeComments(){ 
-  commentsBlock.innerHTML = ''
- }
-
-
-
-
-
-
-
-
-
-
-
-
+function removeComments() {
+  commentsBlock.innerHTML = "";
+  btnComments.removeAttribute("disabled");
+}
